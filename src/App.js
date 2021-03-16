1cb2952ca266ecc531a797c05ae6b224e65d2de6
matylda-bit <58633008+matylda-bit@users.noreply.github.com>
2021-03-16 23:45:42 +0000

    added homepage and directory components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component'
 
-
-function App() {
-  return (<div>
-    <HomePage />
+function App () {
+  return (
+    <div>
+      <HomePage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
